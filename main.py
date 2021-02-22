d997f771798184e363b2f88ed03cfc7d28065e7b
Austingaspr <74085550+Austingaspr@users.noreply.github.com>
2021-02-22 17:01:00 -0600

    Update main.py

--- main.py
+++ main.py
@@ -7,5 +7,5 @@ client = discord.Client()
 client.run('your token here')
 
 
-##This is easy as hell so yeah enjoy lmao!
-##make sure to follow me for more
+# This is easy as hell so yeah enjoy lmao!
+# make sure to follow me for more
